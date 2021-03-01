d78233d896615525db7ff6f778ba4bcbe81c9b7e
Anthony Pack <48550732+anthonyk-pack@users.noreply.github.com>
2021-03-01 17:27:08 +0000

    Upload Project 2 Week 2

---
+++ Week2/s3bucket.py
@@ -0,0 +1,26 @@
+import boto3
+import sys
+
+try:
+    def main():
+        create_s3bucket(TonysBucket447)
+
+except Exception as e:
+    print(e)
+
+def create_s3bucket(TonysBucket447):
+    s3_bucket=boto3.client(
+        's3',
+    )
+
+    bucket = s3_bucket.create_bucket(
+        Bucket=TonysBucket447,
+        ACL='private',
+    )
+
+    print(bucket)
+
+TonysBucket447 = sys.argv[1]
+
+if __name__ == '__main__':
+    main()
